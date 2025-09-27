8e28e278530b7e19170e0fa4601eda009c1b1e75
Sachin-20-s <dasarisachinraj09@gmail.com>
2025-09-27 23:52:59 +0530

    Minor updates

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/talent-flow",
+  // base: process.env.VITE_BASE_PATH || "/talent-flow",
+  base: "/",
   build: {
     outDir: "dist", // Vite default
   },
